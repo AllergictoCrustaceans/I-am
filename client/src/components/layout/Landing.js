58a7a44b37d9b0761c8c775c23b5240ccf490882
Katie Wu <katie.wy.wu@gmail.com>
2019-10-07 10:19:26 -0700

    Add links to "register" and "login" button.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,10 +16,20 @@ class Landing extends Component {
                             Create a (minimal full-stack app with user authentication via passport and JWTs)
                         </p>
                         <br />
+                        <div className = "col s6">
+                            <Link to= "/register" style ={{width: "140px", borderRadius: "3px", letterSpacing: "1.5px"}} className = "btn btn-large waves-effect waves-light hoverable blue accent-3">
+                                Register
+                            </Link>
+                        </div>
+                        <div className = "col s6">
+                            <Link to="/login" style={{width: "140px", borderRadius: "3px", letterSpacing: "1.5px"}} className = "btn btn-large btn-flat waves-effect white black-text">
+                                Login
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
