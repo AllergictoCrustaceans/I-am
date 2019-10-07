e2c590ef267a494477a3470a40e2fb047053fda3
Katie Wu <katie.wy.wu@gmail.com>
2019-10-07 15:53:06 -0700

    Fix typo.

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/private-route/PrivateRoute';
-import Dashboard from './components/dashboard/dashboard';
+import Dashboard from './components/dashboard/Dashboard';
 
 
 //Check for token to keep user logged in
