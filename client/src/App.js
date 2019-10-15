da39c19e0ad2010a88df5178b4759858ea6b47a3
Katie Wu <katie.wy.wu@gmail.com>
2019-10-14 18:01:47 -0700

    Nothing changed.

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import Login from './components/auth/Login';
 import PrivateRoute from './components/private-route/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
 
-
 //Check for token to keep user logged in
 if(localStorage.jwtToken) {
   //Set auth token header auth
