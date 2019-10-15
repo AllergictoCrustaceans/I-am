bf113212ec368da208dc1611a17bf64f53747476
Katie Wu <katie.wy.wu@gmail.com>
2019-10-14 18:02:07 -0700

    Import Facebook and Google components.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import {Link, withRouter} from 'react-router-dom';
+// import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {registerUser} from '../../actions/authActions';
 import classnames from 'classnames';
+import Facebook from './Facebook';
+import Google from './Google';
 
 class Register extends Component {
     constructor() {
@@ -54,109 +57,104 @@ class Register extends Component {
         return (
             <div className = "container" >
                 <div className = "row">
-                    <div className = "col s8 offset-s2">
+                    <div className = "col s12">
                         <Link to="/" className = "btn-flat waves-effect">
-                            <i className = "material-icons left">keyboard_backspace</i> Back to home
+                            <i className = "material-icons left">keyboard_backspace</i> Back to Main Screen
                         </Link>
-
-                        <div className = "col s12" style = {{paddingLeft: "11.250px"}}>
-                            <h4>
-                                <b>Register</b> below
-                            </h4>
-                            <div className = "col s10">
-                                <Link to="/google" className = "btn-flat waves-effect">
-                                    <i className="fab fa-google"></i>
-                                </Link>
-                                
-                                <Link to="/twitter" className = "btn-flat waves-effect">
-                                    <i className="fab fa-twitter"></i>
-                                </Link>
-
-                                <Link to="/facebook" className = "btn-flat waves-effect">
-                                    <i className="fab fa-facebook"></i>
-                                </Link>
+                    </div>
+                </div>
+                <div className = "row">
+                    <div className = "col s6" style = {{paddingLeft: "11.250px"}}>
+                        <h5>
+                            <b>Login with and existing account</b>
+                        </h5>
+                        <div className = "col s10">
+                            <Facebook />
+                            <br/>
+                            <Google />
+                        </div>
+                    </div>
+                    <div className = "col s6">
+                        <h5>
+                            <b>Register Below</b>
+                        </h5>
+                        <form noValidate onSubmit= {this.onSubmit}>
+                            <div className = "input-field col s12">
+                                <input
+                                    onChange = {this.onChange}
+                                    value = {this.state.name}
+                                    error = {errors.name}
+                                    id = "name"
+                                    type = "text"
+                                    className = {classnames ("", {
+                                        invalid: errors.name
+                                    })} />
+                                <label htmlFor = "name">Name</label>
+                                <span className = "red-text">{errors.name}</span>
                             </div>
-                        </div>
-                        <div className = "col s6">
-                            <form noValidate onSubmit= {this.onSubmit}>
-                                <div className = "input-field col s12">
-                                    <input
-                                        onChange = {this.onChange}
-                                        value = {this.state.name}
-                                        error = {errors.name}
-                                        id = "name"
-                                        type = "text"
-                                        className = {classnames ("", {
-                                            invalid: errors.name
-                                        })} />
-                                    <label htmlFor = "name">Name</label>
-                                    <span className = "red-text">{errors.name}</span>
-                                </div>
 
-                                <div className = "input-field col s12">
-                                    <input
-                                        onChange = {this.onChange}
-                                        value = {this.state.email}
-                                        error = {errors.email}
-                                        id = "email"
-                                        type = "email"
-                                        className = {classnames ("", {
-                                            invalid: errors.email
-                                        })} />
-                                    <label htmlFor = "email">Email</label>
-                                    <span className = "red-text">{errors.email}</span>
-                                </div>
+                            <div className = "input-field col s12">
+                                <input
+                                    onChange = {this.onChange}
+                                    value = {this.state.email}
+                                    error = {errors.email}
+                                    id = "email"
+                                    type = "email"
+                                    className = {classnames ("", {
+                                        invalid: errors.email
+                                    })} />
+                                <label htmlFor = "email">Email</label>
+                                <span className = "red-text">{errors.email}</span>
+                            </div>
 
 
-                                <div className = "input-field col s12">
-                                    <input
-                                        onChange = {this.onChange}
-                                        value = {this.state.password}
-                                        error = {errors.password}
-                                        id = "password"
-                                        type = "password"
-                                        className = {classnames ("", {
-                                            invalid: errors.password
-                                        })} />
-                                    <label htmlFor = "password">Password</label>
-                                    <span className = "red-text">{errors.password}</span>
-                                </div>
+                            <div className = "input-field col s12">
+                                <input
+                                    onChange = {this.onChange}
+                                    value = {this.state.password}
+                                    error = {errors.password}
+                                    id = "password"
+                                    type = "password"
+                                    className = {classnames ("", {
+                                        invalid: errors.password
+                                    })} />
+                                <label htmlFor = "password">Password</label>
+                                <span className = "red-text">{errors.password}</span>
+                            </div>
 
-                                <div className = "input-field col s12">
-                                    <input
-                                        onChange = {this.onChange}
-                                        value = {this.state.password2}
-                                        error = {errors.password2}
-                                        id = "password2"
-                                        type = "password" 
-                                        className = {classnames ("", {
-                                            invalid: errors.password2
-                                        })}/>
-                                    <label htmlFor = "password2">Confirm Password</label>
-                                    <span className = "red-text">{errors.password2}</span>
-                                </div>
+                            <div className = "input-field col s12">
+                                <input
+                                    onChange = {this.onChange}
+                                    value = {this.state.password2}
+                                    error = {errors.password2}
+                                    id = "password2"
+                                    type = "password" 
+                                    className = {classnames ("", {
+                                        invalid: errors.password2
+                                    })}/>
+                                <label htmlFor = "password2">Confirm Password</label>
+                                <span className = "red-text">{errors.password2}</span>
+                            </div>
 
-                                <div className = "col s12" style = {{paddingLeft: "11.250px"}}>
-                                    <button 
-                                        style = {{
-                                            width: "150px",
-                                            borderRadius: "150px",
-                                            letterSpacing: "1.5px",
-                                            marginTop: "1rem"
-                                        }}
-                                        type = "Submit"
-                                        className = "btn btn-large waves-effect waves-light hoverable blue accent-3">
-                                        Sign Up
-                                    </button>
+                            <div className = "col s12" style = {{paddingLeft: "11.250px"}}>
+                                <button 
+                                    style = {{
+                                        width: "150px",
+                                        borderRadius: "150px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type = "Submit"
+                                    className = "btn btn-large waves-effect waves-light hoverable blue accent-3">
+                                    Sign Up
+                                </button>
 
-                                    <p className = "grey-text text-darken-1">
-                                    Already have an account? 
-                                    <Link to="/login"> Login</Link>
-                                    </p>
-                                </div>
-                            </form>
-                        </div>
-                        
+                                <p className = "grey-text text-darken-1">
+                                Already have an account? 
+                                <Link to="/login"> Login</Link>
+                                </p>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
