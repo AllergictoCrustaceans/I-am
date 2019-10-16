4087c1dc73cea0ff27e9295a70d5119fa1da7884
Katie Wu <katie.wy.wu@gmail.com>
2019-10-15 18:18:58 -0700

    Create empty links to settings and help buttons.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,9 +17,14 @@ class Navbar extends Component {
 
 
                         {/* Setting Link */}
-                        
+                        <Link to="/settings" style={{width: "2px", borderRadius: "20px"}} className = "btn btn-small waves-effect waves-light hoverable blue accent-3">
+                            {/* animate blue pulse */}
+                        </Link>
 
                         {/* Help Link */}
+                        <Link to="/help" style={{width: "2px", borderRadius: "20px"}} className = "btn btn-small waves-effect waves-light hoverable purple accent-3">
+                            {/* animate purple pulse  */}
+                        </Link>
                     </div>
                 </nav>
             </div>
