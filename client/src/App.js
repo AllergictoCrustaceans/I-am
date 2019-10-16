c449b6f738aef39e719d1b8fc281f93b433a2695
Katie Wu <katie.wy.wu@gmail.com>
2019-10-15 18:17:47 -0700

    Import link library from react-router-dom.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import {setCurrentUser, logoutUser} from './actions/authActions';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -41,18 +42,17 @@ if(localStorage.jwtToken) {
 
 class App extends Component {
   render () {
-    // Settings and help buttons 
     return (
       <Provider store = {store}>
         <Router>
           <div className = "App">
             <Navbar />
-            <Route exact path = "/" component = {Landing} />
+            {/* <Route exact path = "/" component = {Landing} />
             <Route exact path = "/register" component = {Register} />
             <Route exact path = "/login" component = {Login} />
             <Switch>
               <PrivateRoute exact path = "/dashboard" component = {Dashboard} />
-            </Switch>
+            </Switch> */}
           </div>
         </Router>
       </Provider>
