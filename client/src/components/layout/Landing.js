7d665b1f841b11fec35aee8eedbbb44934d7186a
Katie Wu <katie.wy.wu@gmail.com>
2019-10-16 14:35:40 -0700

    Remove comment and bootstrap class on register button.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,16 +7,10 @@ class Landing extends Component {
             <div style = {{height: "75vh"}} className = "container valign-wrapper">
                 <div className = "row">
                     <div className = "col s12 center-align">
-                        {/* <h4>
-                            I solemnly swear <b>I am up to no good</b> 
-                        </h4>
-                        <p className = "flow-text grey-text text-darken-1">
-                            Subtitle
-                        </p> */}
                         {/* Backgorund with blender */}
                         <br />
                         <div className = "col s6">
-                            <Link to= "/register" style ={{width: "140px", borderRadius: "3px", letterSpacing: "1.5px"}} className = "btn btn-large waves-effect waves-light hoverable blue accent-3">
+                            <Link to= "/register" style ={{width: "140px", borderRadius: "3px", letterSpacing: "1.5px"}} className = "btn btn-large btn-flat waves-effect white black-text">
                                 Register
                             </Link>
                         </div>
