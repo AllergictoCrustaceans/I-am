af9c35aae31312c39db2a07fd7aad3dc3a239a61
Katie Wu <katie.wy.wu@gmail.com>
2019-10-16 10:43:57 -0700

    Uncomment components for debugging.

--- client/src/App.js
+++ client/src/App.js
@@ -47,12 +47,12 @@ class App extends Component {
         <Router>
           <div className = "App">
             <Navbar />
-            {/* <Route exact path = "/" component = {Landing} />
+            <Route exact path = "/" component = {Landing} />
             <Route exact path = "/register" component = {Register} />
             <Route exact path = "/login" component = {Login} />
             <Switch>
               <PrivateRoute exact path = "/dashboard" component = {Dashboard} />
-            </Switch> */}
+            </Switch>
           </div>
         </Router>
       </Provider>
