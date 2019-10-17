53a0004682623346cab3b820e11018152d767df2
Katie Wu <katie.wy.wu@gmail.com>
2019-10-17 13:32:42 -0700

    Created topic Schema.

---
+++ backend/models/Topic.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+let topicSchema = new Schema (
+    {
+        userInput: {
+            type: String,
+            required: true
+        },
+        topicGroup: {
+            type: String,
+            required: true
+        },
+        keywords: {
+            type: String,
+            required: true
+        },
+        Weight: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Topic = mongoose('Topic', topicSchema, 'Topic');
+
+module.exports = Topic;
