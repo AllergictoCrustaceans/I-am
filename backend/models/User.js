65f035568d1e83bebe178b5b9db86be2046e877f
Katie Wu <katie.wy.wu@gmail.com>
2019-10-22 11:12:44 -0700

    Indent fields.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UserSchema = new Schema ({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
+const UserSchema = new Schema (
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }   
     }
-});
+);
 
 //
 //DOUBLE CHECK
