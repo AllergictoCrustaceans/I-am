80ee3a2c7ec63afbee2b005e8d9cb4325b017c41
Katie Wu <katie.wy.wu@gmail.com>
2019-10-22 16:01:42 -0700

    Comment out mongodb query.

--- backend/routes/api/moodNTopic.js
+++ backend/routes/api/moodNTopic.js
@@ -3,10 +3,19 @@ const router = express.Router();
 let topic = require('../../models/Topic');
 let chatlog = require('../../models/ChatLog');
 
-//combine chatlog and topic database together
-//assign colors to the sentiments.
+// //combine chatlog and topic database together
+// //assign colors to the sentiments.
+// router.ChatLog.find({
+// 	"$where": "this.CT Chatlog.botInput, Chatlog.userInput, Chatlog.date FROM ChatLog INNER JOIN User as User ON User.name  == this. Chatlog.name "
+// },{
+// 	"Chatlog.botInput": 1,
+// 	"Chatlog.userInput": 1,
+// 	"Chatlog.date": 1
+// }
+// ).sort({
+// 	"Chatlog.date": 1
+// });
 
+// router.get('/mood', (req, res) => {
 
-router.get('/mood', (req, res) => {
-
-})
+// })
