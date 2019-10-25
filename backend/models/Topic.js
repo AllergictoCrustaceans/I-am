13f56dec64e12d8f6280375c70f647025e2057a1
Katie Wu <katie.wy.wu@gmail.com>
2019-10-25 15:31:11 -0700

    Add userID as field, comment out weight and topicGroup fields.

--- backend/models/Topic.js
+++ backend/models/Topic.js
@@ -2,22 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let topicSchema = new Schema (
     {
+        userID: {
+            type: String,
+            required: true
+        },
         userInput: {
             type: String,
             required: true
         },
-        topicGroup: {
-            type: String,
-            required: true
-        },
+        // topicGroup: {
+        //     type: String,
+        //     required: true
+        // },
         keywords: {
             type: String,
             required: true
         },
-        Weight: {
-            type: String,
-            required: true
-        }
+        // weight: {
+        //     type: String,
+        //     required: true
+        // }
     },
     {
         timestamps: true
