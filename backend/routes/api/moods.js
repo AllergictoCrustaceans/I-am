c92bc2a9ee0febdd8850dc926580dae01e9dcbd9
Katie Wu <katie.wy.wu@gmail.com>
2019-11-06 17:22:01 -0800

    Temporarily comment out moods.js queries for debugging.

--- backend/routes/api/moods.js
+++ backend/routes/api/moods.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-west-2'}); 
+// const AWS = require('aws-sdk');
+// AWS.config.update({region: 'us-west-2'}); 
 
-const documentClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+// const documentClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 
-async function getUserTopic() {
-    router.get('/moods', (req, res) => {
-        const getTopicParams = {
-            TableName : 'Messages',
-            FilterExpression: '#t = :t AND #n = :n',
-            ExpressionAttributeNames: {
-                '#t' : 'Topic',
-                '#n' : 'Name'
-            },
-            ExpressionAttributeValues: {
-                // ":n" : the oauth email that the user is assocated to
-                // ":t" : the topic name that the user selected on screen
-            },
-        }
+// async function getUserTopic() {
+//     router.get('/moods', (req, res) => {
+//         const getTopicParams = {
+//             TableName : 'Messages',
+//             FilterExpression: '#t = :t AND #n = :n',
+//             ExpressionAttributeNames: {
+//                 '#t' : 'Topic',
+//                 '#n' : 'Name'
+//             },
+//             ExpressionAttributeValues: {
+//                 // ":n" : the oauth email that the user is assocated to
+//                 // ":t" : the topic name that the user selected on screen
+//             },
+//         }
 
-        const data = await documentClient.scan(getTopicParams).promise();
-        try {
-            console.log(data);
-        } catch(err) {
-            console.log(err);
-        }
-    })  
-}
+//         const data = await documentClient.scan(getTopicParams).promise();
+//         try {
+//             console.log(data);
+//         } catch(err) {
+//             console.log(err);
+//         }
+//     })  
+// }
 
-exports.handler = getUserTopic;
-module.exports = router;
+// exports.handler = getUserTopic;
+// module.exports = router;
