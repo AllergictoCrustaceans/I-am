ac41d473b6b1a7b8d539c9f3ed8b152207c0e308
Katie Wu <katie.wy.wu@gmail.com>
2019-11-09 15:39:33 -0800

    Create basic Oauth comonent.

--- client/src/components/Oauth.js
+++ client/src/components/Oauth.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './App.css';
-import {Auth} from 'aws-amplify';
+import React, {useEffect}from 'react';
+import '../App.css';
+import {Auth, Hub} from 'aws-amplify';
+
+function checkUser() {
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log({ user }))
+      .catch(err => console.log(err))
+  }
+  
+  function signOut() {
+    Auth.signOut()
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
 
 function Oauth(props) {
+    useEffect(() => {
+        Hub.listen('auth', (data) => {
+          const { payload } = data
+          console.log('A new auth event has happened: ', data)
+           if (payload.event === 'signIn') {
+             console.log('a user has signed in!')
+           }
+           if (payload.event === 'signOut') {
+             console.log('a user has signed out!')
+           }
+        })
+      }, [])
     return (
         <div className="App">
           <header className="App-header">
             <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+            <button onClick={checkUser}>Check User</button>
+            <button onClick={signOut}>Sign Out</button>
+            <button onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}>Sign In with Facebook</button>
+            <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign In with Google</button>
+    
           </header>
         </div>
-      )
+    )
 }
 
 export default Oauth;
