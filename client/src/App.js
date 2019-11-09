e304b3b7e5c5758666e2754b38025d3abcbf1124
Katie Wu <katie.wy.wu@gmail.com>
2019-11-09 14:06:53 -0800

    Comment out old code to  test serverless comonents.

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,43 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser, logoutUser} from './actions/authActions';
-import './App.css';
+// import jwt_decode from 'jwt-decode';
+// import setAuthToken from './utils/setAuthToken';
+// import {setCurrentUser, logoutUser} from './actions/authActions';
 
-import {Provider} from 'react-redux';
-import store from './store';
+import Oauth from './components/Oauth';
 
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import PrivateRoute from './components/private-route/PrivateRoute';
-import Dashboard from './components/dashboard/Dashboard';
+// import {Provider} from 'react-redux';
+// import store from './store';
+
+// import Navbar from './components/layout/Navbar';
+// import Landing from './components/layout/Landing';
+// import Register from './components/auth/Register';
+// import Login from './components/auth/Login';
+// import PrivateRoute from './components/private-route/PrivateRoute';
+// import Dashboard from './components/dashboard/Dashboard';
 
 //Check for token to keep user logged in
-if(localStorage.jwtToken) {
-  //Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
+// if(localStorage.jwtToken) {
+//   //Set auth token header auth
+//   const token = localStorage.jwtToken;
+//   setAuthToken(token);
   
-  //Decode token and get user info and exp
-  const decoded = jwt_decode(token);
+//   //Decode token and get user info and exp
+//   const decoded = jwt_decode(token);
 
-  //Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
+//   //Set user and isAuthenticated
+//   store.dispatch(setCurrentUser(decoded));
 
-  //Check for expired token
-  const currentTime = Date.now() / 1000; //to get in milliseconds 
-  if(decoded.exp < currentTime) {
-    //Logout user
-    store.dispatch(logoutUser());
+//   //Check for expired token
+//   const currentTime = Date.now() / 1000; //to get in milliseconds 
+//   if(decoded.exp < currentTime) {
+//     //Logout user
+//     store.dispatch(logoutUser());
 
-    //Redirect to login
-    window.location.href = "./login";
-  }
-}
+//     //Redirect to login
+//     window.location.href = "./login";
+//   }
+// }
 
 
 class App extends Component {
@@ -45,13 +46,14 @@ class App extends Component {
       <Provider store = {store}>
         <Router>
           <div className = "App">
-            <Navbar />
-            <Route exact path = "/" component = {Landing} />
-            <Route exact path = "/register" component = {Register} />
-            <Route exact path = "/login" component = {Login} />
-            <Switch>
+            <Oauth />
+            {/* <Navbar /> */}
+            {/* <Route exact path = "/" component = {Landing} /> */}
+            {/* <Route exact path = "/register" component = {Register} /> */}
+            {/* <Route exact path = "/login" component = {Login} /> */}
+            {/* <Switch>
               <PrivateRoute exact path = "/dashboard" component = {Dashboard} />
-            </Switch>
+            </Switch> */}
           </div>
         </Router>
       </Provider>
