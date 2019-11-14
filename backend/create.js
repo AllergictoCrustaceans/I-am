545ed078e544a96707506397aef6eca0c43b7024
Katie Wu <katie.wy.wu@gmail.com>
2019-11-13 23:35:22 -0800

    Revise "userId" to "userID", and fix trailing whitespace.

--- backend/create.js
+++ backend/create.js
@@ -1,52 +1,26 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import {success, failure} from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context) {
   const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.tableName,
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      messageId: uuid.v1(),
+      userID: event.requestContext.identity.cognitoIdentityId,
+      messageID: uuid.v1(),
       userInput: data.content,
-    //   botInput:
-    //   overallSentiment:
-    //   positive:
-    //   mixed:
-    //   neutral:
-    //   negative:
       attachment: data.attachment,
       createdAt: Date.now()
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 }
