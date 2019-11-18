20cbbc4b5363f7891c48ee7114689c1432aee2ca
Katie Wu <katie.wy.wu@gmail.com>
2019-11-18 15:28:49 -0800

    Attempt at modifying the create.js to gather values from Lex for dynamo to store.

--- backend/create.js
+++ backend/create.js
@@ -1,24 +1,40 @@
 import uuid from "uuid";
+import AWS from 'aws-sdk';
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import {success, failure} from "./libs/response-lib";
 
+const comprehend = new AWS.Comprehend();
+
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
+  // const data = JSON.parse(event.body);
 
   const params = {
+    Text: event.inputTranscript,
+    LanguageCode: 'en'
+  };
+
+  const response = await comprehend.detectSentiment(params).promise();
+  console.log(response);
+  let sentiment;
+
+  const putParams = {
     TableName: process.env.tableName,
     Item: {
       userID: event.requestContext.identity.cognitoIdentityId,
       messageID: uuid.v1(),
-      userInput: data.content,
-      attachment: data.attachment,
+      userInput: params.Text, //data.content,
+      botInput: sentiment.dialogAction.message.content,
+      positive: response.SentimentScore.Positive,
+      neutral: response.SentimentScore.Neutral,
+      mixed: response.SentimentScore.Mixed,
+      negative: response.SentimentScore.Negative,
       createdAt: Date.now()
     }
   };
 
   try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
+    await dynamoDbLib.call("put", putParams);
+    return success(putParams.Item);
   } catch (e) {
     console.log(e);
     return failure({ status: false });
