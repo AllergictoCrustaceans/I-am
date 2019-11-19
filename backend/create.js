17072c48da1dc80fd6895d21b2fab9f2e1024a7a
Katie Wu <katie.wy.wu@gmail.com>
2019-11-18 22:04:46 -0800

    Modify create.js to be connected to Lex and dynamo.

--- backend/create.js
+++ backend/create.js
@@ -1,42 +1,99 @@
-import uuid from "uuid";
-import AWS from 'aws-sdk';
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import {success, failure} from "./libs/response-lib";
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-2'});
 
 const comprehend = new AWS.Comprehend();
+const documentClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 
-export async function main(event, context) {
-  // const data = JSON.parse(event.body);
+async function main(event, context) {
+    console.log("******************************************", event);
+    const params = {
+        Text: event.currentIntent.slots.thoughts,
+        LanguageCode: 'en'
+    };
 
-  const params = {
-    Text: event.inputTranscript,
-    LanguageCode: 'en'
-  };
+    const response = await comprehend.detectSentiment(params).promise();
+    console.log("*************************************", response);
+    let sentiment;
 
-  const response = await comprehend.detectSentiment(params).promise();
-  console.log(response);
-  let sentiment;
-
-  const putParams = {
-    TableName: process.env.tableName,
-    Item: {
-      userID: event.requestContext.identity.cognitoIdentityId,
-      messageID: uuid.v1(),
-      userInput: params.Text, //data.content,
-      botInput: sentiment.dialogAction.message.content,
-      positive: response.SentimentScore.Positive,
-      neutral: response.SentimentScore.Neutral,
-      mixed: response.SentimentScore.Mixed,
-      negative: response.SentimentScore.Negative,
-      createdAt: Date.now()
+    if(response.Sentiment === "POSITIVE") {
+        sentiment = {
+            "sessionAttributes" : {
+                "sentiment" : response.Sentiment
+            },
+            "dialogAction" : {
+                "type" : "Close",
+                "fulfillmentState" : "Fulfilled",
+                "message" : {
+                    "contentType" : "PlainText",
+                    "content" : "Wow! That's something to celebrate. :) Do you feel like this incident taught you something new about yourself?"
+                }
+            }
+        };
+    } else if(response.Sentiment === "MIXED") {
+        sentiment = {
+            "sessionAttributes" : {
+                "sentiment" : response.Sentiment
+            },
+            "dialogAction" : {
+                "type" : "Close",
+                "fulfillmentState" : "Fulfilled",
+                "message" : {
+                    "contentType" : "PlainText",
+                    "content" : "I'm not sure how I feel about this either. The good and the bad parts are necessary, don't ya think?"
+                }
+            }
+        };
+    } else if(response.Sentiment === "NEUTRAL") {
+        sentiment = {
+            "sessionAttributes" : {
+                "sentiment" : response.Sentiment
+            },
+            "dialogAction" : {
+                "type" : "Close",
+                "fulfillmentState" : "Fulfilled",
+                "message" : {
+                    "contentType" : "PlainText",
+                    "content" : "I understand. Hey-- but you know what? There's nothing wrong with feeling neutral about this. That's what makes us human. :) "
+                }
+            }
+        };
+    } else {
+        sentiment = {
+            "sessionAttributes" : {
+                "sentiment" : response.Sentiment
+            },
+            "dialogAction" : {
+                "type" : "Close" ,
+                "fulfillmentState" : "Fulfilled",
+                "message" : {
+                    "contentType" : "PlainText",
+                    "content" : "Ooof. That sounds rough :/ How have you been holding up since this happened?"
+                }
+            }
+        };
     }
-  };
+        const putParams = {
+            TableName: 'UsersMessages',
+            Item: {
+                "userID": event.currentIntent.slots.name,
+                "messageID": uuid.v1(),
+                "timestamp": Date.now(),
+                "userInput": params.Text,
+                "botInput": sentiment.dialogAction.message.content,
+                "topic" : event.currentIntent.slots.subject,
+                "overallSentiment": response.Sentiment,
+                "positive": response.SentimentScore.Positive,
+                "neutral": response.SentimentScore.Neutral,
+                "mixed": response.SentimentScore.Mixed,
+                "negative": response.SentimentScore.Negative
+            },
+        };
+        console.log("***************************************", sentiment);
 
-  try {
-    await dynamoDbLib.call("put", putParams);
-    return success(putParams.Item);
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false });
-  }
+
+        await documentClient.put(putParams).promise();
+    return sentiment;
 }
+
+exports.main = main;
