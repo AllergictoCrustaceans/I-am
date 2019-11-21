d0e1ee66f7505e873d9d8b2fea8cc4e8a60a6276
Katie Wu <katie.wy.wu@gmail.com>
2019-11-20 21:54:19 -0800

    Update get.js to dynamo's query method.

--- backend/get.js
+++ backend/get.js
@@ -2,26 +2,28 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+  console.log(event);
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      userID: event.requestContext.identity.cognitoIdentityId,
-      messageID: event.pathParameters.id
+    KeyConditionExpression: '#userID =:userID',
+    ExpressionAttributeNames: {
+      '#userID' : 'userID'
+    },
+    ExpressionAttributeValues: {
+      ':userID' : event.queryStringParameters.email
     }
   };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
+    const result = await dynamoDbLib.call("query", params);
+    if (result.Items) {
       // Return the retrieved item
-      return success(result.Item);
+      return success(result.Items);
     } else {
       return failure({ status: false, error: "Item not found." });
     }
   } catch (e) {
+    console.log(e);
     return failure({ status: false });
   }
 }
