4e56069c9c21cc44e8c2831e21b907d1f8964f9b
Katie Wu <katie.wy.wu@gmail.com>
2019-11-20 21:55:08 -0800

    Change list.js, and update params key values.

--- backend/list.js
+++ backend/list.js
@@ -10,6 +10,7 @@ export async function main(event, context) {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
+    ProjectionExpression: "#userID, topic, overallSentiment, positive, negative, mixed, neutral",
     KeyConditionExpression: "userID = :userID",
     ExpressionAttributeValues: {
       ":userID": event.requestContext.identity.cognitoIdentityId
